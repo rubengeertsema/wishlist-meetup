453585588d7eabf5d85e043cd4cade0c1855560e
rubengeertsema <rubengeertsema@gmail.com>
2017-10-01 21:22:18 +0200

    Added projectlombok

--- backend/src/main/java/nl/demo/wishlist/model/Wish.java
+++ backend/src/main/java/nl/demo/wishlist/model/Wish.java
@@ -1,5 +1,8 @@
 package nl.demo.wishlist.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,47 +11,12 @@ import java.util.Date;
 /**
  * Created by rubengeertsema on 16/04/2017.
  */
+@NoArgsConstructor
 @Document(collection = "wishes")
 public class Wish {
 
-    @Id
-    private String id;
-    private String title;
-    private String description;
-    private Date date;
-
-    public Wish() {
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
+    @Id @Getter @Setter private String id;
+    @Getter @Setter private String title;
+    @Getter @Setter private String description;
+    @Getter @Setter private Date date;
 }
